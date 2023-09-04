294bbc9748116d96c522e248467101f24d4f3d89
Groldi <chniucg@gmail.com>
2023-09-04 20:07:26 +0200

    Removed pufferfish out of upsteamCommit

--- KissenPaper/scripts/upstreamCommit.sh
+++ KissenPaper/scripts/upstreamCommit.sh
@@ -2,9 +2,8 @@
 
 # requires curl & jq
 
-# upstreamCommit --paper HASH --pufferfish HASH
+# upstreamCommit --paper HASH
 # flag: --paper HASH - (Optional) the commit hash to use for comparing commits between paper (PaperMC/Paper/compare/HASH...HEAD)
-# flag: --pufferfish HASH - the commit hash to use for comparing commits between pufferfish (pufferfish-gg/Pufferfish/compare/HASH...HEAD)
 
 function getCommits() {
     curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$1"/compare/"$2"..."$3" | jq -r '.commits[] | "'"$1"'@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"'
@@ -15,18 +14,13 @@ set -e
 PS1="$"
 
 paperHash=$(git diff gradle.properties | awk '/^-paperCommit =/{print $NF}')
-pufferfishHash=""
 
-TEMP=$(getopt --long paper:,pufferfish: -o "" -- "$@")
+TEMP=$(getopt --long paper: -o "" -- "$@")
 eval set -- "$TEMP"
 while true; do
     case "$1" in
         --paper)
             paperHash="$2"
-            shift 2
-            ;;
-        --pufferfish)
-            pufferfishHash="$2"
             shift 2
             ;;
         *)
@@ -36,8 +30,6 @@ while true; do
 done
 
 paper=""
-pufferfish=""
-updated=""
 logsuffix=""
 
 # Paper updates
@@ -47,32 +39,12 @@ if [ -n "$paperHash" ]; then
 
     # Updates found
     if [ -n "$paper" ]; then
-        updated="Paper"
         logsuffix="$logsuffix\n\nPaper Changes:\n$paper"
     fi
 fi
 
-# Pufferfish updates
-if [ -n "$pufferfishHash" ]; then
-    pufferfish=$(getCommits "pufferfish-gg/Pufferfish" "$pufferfishHash" "HEAD")
-
-    # Updates found
-    if [ -n "$pufferfish" ]; then
-        updated="Pufferfish"
-        logsuffix="$logsuffix\n\nPufferfish Changes:\n$pufferfish"
-    fi
-fi
-
-# Both have updates
-if [ -n "$paper" ] && [ -n "$pufferfish" ]; then
-    updated="Paper & Pufferfish"
-fi
-
 disclaimer="Upstream has released updates that appear to apply and compile correctly"
-log="Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
-
-git add gradle.properties
+log="Updated Upstream (Paper)\n\n${disclaimer}${logsuffix}"
 
 echo -e "$log" | git commit -F -
-
 ) || exit 1
