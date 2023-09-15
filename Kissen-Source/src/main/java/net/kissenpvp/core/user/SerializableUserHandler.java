d9068a28e487d63d71c54fd3b7a7dbe3d5e250ed
Groldi <chniucg@gmail.com>
2023-09-15 22:59:40 +0200

    Updated paper and adjusted code conventions to prevent headaches

--- Kissen-Source/src/main/java/net/kissenpvp/core/user/SerializableUserHandler.java
+++ Kissen-Source/src/main/java/net/kissenpvp/core/user/SerializableUserHandler.java
@@ -1,17 +1,19 @@
 /*
- * Copyright 2023 KissenPvP
+ * Copyright (C) 2023 KissenPvP
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is licensed under the Apache License, Version 2.0.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * This software may be redistributed and/or modified under the terms
+ * of the Apache License as published by the Apache Software Foundation,
+ * either version 2 of the License, or (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This program is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the Apache
+ * License, Version 2.0 for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * You should have received a copy of the Apache License, Version 2.0
+ * along with this program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
  */
 
 package net.kissenpvp.core.user;
