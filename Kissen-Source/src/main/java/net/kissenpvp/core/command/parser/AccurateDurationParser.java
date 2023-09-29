4650b18560aca5a9fbda457e2691581847d2828b
Groldi <chniucg@gmail.com>
2023-09-29 20:23:22 +0200

    Improve error handling in AccurateDurationParser
    Added a DateTimeParseException in AccurateDurationParser.java to handle cases where input string does not contain required characters (Y, W, D, H, M, S). This was done to prevent errors during the conversion process and to provide a more understandable error message for failed conversions.

--- Kissen-Source/src/main/java/net/kissenpvp/core/command/parser/AccurateDurationParser.java
+++ Kissen-Source/src/main/java/net/kissenpvp/core/command/parser/AccurateDurationParser.java
@@ -7,6 +7,8 @@ import net.kissenpvp.core.api.time.TimeImplementation;
 import net.kissenpvp.core.base.KissenCore;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.format.DateTimeParseException;
+
 public class AccurateDurationParser<S extends ServerEntity> implements ArgumentParser<AccurateDuration, S> {
     @Override
     public @NotNull String serialize(@NotNull AccurateDuration object) {
@@ -15,6 +17,11 @@ public class AccurateDurationParser<S extends ServerEntity> implements ArgumentP
 
     @Override
     public @NotNull AccurateDuration deserialize(@NotNull String input) {
+
+        if(!input.matches("(?i).*[ywdhms].*"))
+        {
+            throw new DateTimeParseException("Input doesn't contain the required characters (Y, W, D, H, M, S).", input, input.length());
+        }
         return KissenCore.getInstance().getImplementation(TimeImplementation.class).parse(input);
     }
 }
