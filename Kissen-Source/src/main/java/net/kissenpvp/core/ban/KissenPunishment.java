7fc85e7d9d86033ed0bf84c74efa98ebc824ea37
Ivo <chniucg@gmail.com>
2023-10-18 19:52:57 +0200

    Made Name of ban operator change dynamically
    Signed-off-by: Ivo <chniucg@gmail.com>

--- Kissen-Source/src/main/java/net/kissenpvp/core/ban/KissenPunishment.java
+++ Kissen-Source/src/main/java/net/kissenpvp/core/ban/KissenPunishment.java
@@ -26,6 +26,8 @@ import net.kissenpvp.core.api.event.EventImplementation;
 import net.kissenpvp.core.api.message.Comment;
 import net.kissenpvp.core.api.networking.client.entitiy.PlayerClient;
 import net.kissenpvp.core.api.networking.client.entitiy.ServerEntity;
+import net.kissenpvp.core.api.user.UserImplementation;
+import net.kissenpvp.core.api.user.UserInfo;
 import net.kissenpvp.core.ban.events.punishment.PunishmentAlterCauseEvent;
 import net.kissenpvp.core.ban.events.punishment.PunishmentAlterEndEvent;
 import net.kissenpvp.core.ban.events.punishment.PunishmentCommentEvent;
@@ -77,6 +79,12 @@ public abstract class KissenPunishment<T> extends KissenTemporalObject implement
 
     @Override
     public @NotNull String getBanOperator() {
+
+        String data = kissenPunishmentNode.operator();
+        if(data.matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$"))
+        {
+            return KissenCore.getInstance().getImplementation(UserImplementation.class).getCachedUserProfile(UUID.fromString(data)).map(UserInfo::getName).orElse(data);
+        }
         return kissenPunishmentNode.operator();
     }
 
