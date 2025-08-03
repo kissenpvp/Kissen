3b2a8136497aa649cf5de0ea9e53d2d9b84c03b6
Ivo Quiring <falk.quiring@gmail.com>
2025-08-03 12:55:36 +0200

    Remove unused import

--- kissen-source/src/main/java/net/kissenpvp/database/InternalConnectionProvider.java
+++ kissen-source/src/main/java/net/kissenpvp/database/InternalConnectionProvider.java
@@ -4,7 +4,6 @@ import net.kissenpvp.api.database.ConnectionProvider;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
