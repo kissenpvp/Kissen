4ed9aec83a12285d5bfb533e7a715e159c7d54d6
Ivo Quiring <falk.quiring@gmail.com>
2025-08-09 01:31:16 +0200

    Remove main class

--- kissen-source/build.gradle.kts
+++ kissen-source/build.gradle.kts
@@ -10,8 +10,4 @@ dependencies {
 tasks.jar {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-
-    manifest {
-        attributes["Main-Class"] = "your.main.Class" // If you have a main class
-    }
 }
