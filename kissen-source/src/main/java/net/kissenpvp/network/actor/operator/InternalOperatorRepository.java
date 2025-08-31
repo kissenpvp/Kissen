ccf25c9abac08c75a48520197ac9c4365d6f3987
Ivo Quiring <falk.quiring@gmail.com>
2025-08-31 12:25:14 +0200

    Add `username` field to `OperatorInfo` and update `InternalOperatorRepository` and schema migration accordingly

--- kissen-source/src/main/java/net/kissenpvp/network/actor/operator/InternalOperatorRepository.java
+++ kissen-source/src/main/java/net/kissenpvp/network/actor/operator/InternalOperatorRepository.java
@@ -10,11 +10,34 @@ import java.sql.Types;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * An abstract repository class specifically designed to manage {@link OperatorInfo} entities
+ * within a relational database.
+ * <p>
+ * This class provides fundamental data access operations
+ * for a table containing operator information and handles the mapping between database rows
+ * and {@link OperatorInfo} objects.
+ * <p>
+ * This repository extends the {@link InternalRepository} class, inheriting base functionality
+ * for database interaction, while overriding and implementing specific behavior to
+ * transform database rows into {@link OperatorInfo} entities.
+ *
+ * @author Ivo Quiring
+ */
 public abstract class InternalOperatorRepository extends InternalRepository<UUID, OperatorInfo>
 {
+    /**
+     * Constructs a new {@code InternalOperatorRepository} instance with the specified table name and database connection.
+     * This class provides repository functionalities for managing operator information in a specified database table.
+     *
+     * @param table      the name of the database table to use; must not be null, blank, or invalid.
+     * @param connection the database connection to use; must not be null.
+     * @throws NullPointerException     if the {@code table} or {@code connection} is {@code null}.
+     * @throws IllegalArgumentException if the {@code table} name is blank, too long, or does not match the required pattern.
+     */
     public InternalOperatorRepository(@NotNull String table, @NotNull Connection connection) throws NullPointerException
     {
-        super(table, connection, "SELECT op_level, can_bypass_player_limit FROM %s WHERE id = ?;");
+        super(table, connection, "SELECT username, op_level, can_bypass_player_limit FROM %s WHERE id = ?;");
     }
 
     @Override
@@ -29,16 +52,20 @@ public abstract class InternalOperatorRepository extends InternalRepository<UUID
     @Override
     public @NotNull CompletableFuture<Void> saveAll(@NotNull Iterable<OperatorInfo> id) throws NullPointerException
     {
-        String sql = "INSERT INTO %s (id, op_level, can_bypass_player_limit) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE op_level = ?, can_bypass_player_limit = ?;";
+        String sql = "INSERT INTO %s (id, username, op_level, can_bypass_player_limit) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE username = ?, op_level = ?, can_bypass_player_limit = ?;";
         return CompletableFuture.supplyAsync(() -> query(sql, (statement ->
         {
-            for(OperatorInfo operatorInfo : id)
+            for (OperatorInfo operatorInfo : id)
             {
                 statement.setString(1, String.valueOf(operatorInfo.id()));
-                setDual(statement, 2, 4, Types.INTEGER, operatorInfo.getLevel());
-                setDual(statement, 3, 5, Types.BOOLEAN, operatorInfo.getBypassesPlayerLimit());
+
+                setDual(statement, 2, 5, Types.VARCHAR, operatorInfo.name());
+                setDual(statement, 3, 6, Types.INTEGER, operatorInfo.getLevel());
+                setDual(statement, 4, 7, Types.BOOLEAN, operatorInfo.getBypassesPlayerLimit());
+
                 statement.addBatch();
             }
+
             statement.executeBatch();
             return null;
         })));
--- kissen-source/src/main/java/net/kissenpvp/network/actor/operator/OperatorInfo.java
+++ kissen-source/src/main/java/net/kissenpvp/network/actor/operator/OperatorInfo.java
@@ -1,12 +1,38 @@
 package net.kissenpvp.network.actor.operator;
 
 import net.kissenpvp.api.database.PersistableEntity;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
+/**
+ * Represents an operator with identifiable properties and privileges within the system.
+ * <p>
+ * This entity provides methods to access the operator's name, operational level, and privilege
+ * to bypass player limits.
+ *
+ * @author Ivo Quiring
+ */
 public interface OperatorInfo extends PersistableEntity<UUID>
 {
+    /**
+     * Retrieves the name of the operator.
+     *
+     * @return a non-null string representing the operator's name.
+     */
+    @NotNull String name();
+
+    /**
+     * Retrieves the operational level of the operator.
+     *
+     * @return an integer representing the operator's level, typically used to determine authority or privilege within the system.
+     */
     int getLevel();
 
+    /**
+     * Determines whether the operator has the privilege to bypass the player limit.
+     *
+     * @return true if the operator can bypass the player limit, false otherwise.
+     */
     boolean getBypassesPlayerLimit();
 }
