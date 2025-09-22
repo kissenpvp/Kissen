a2dc67436cb33beb598437f386383dfbd81c23e2
Ivo Quiring <falk.quiring@gmail.com>
2025-09-22 20:33:08 +0200

    Make connection provider publicly available

--- kissen-api/src/main/java/net/kissenpvp/api/base/Kissen.java
+++ kissen-api/src/main/java/net/kissenpvp/api/base/Kissen.java
@@ -1,11 +1,14 @@
 package net.kissenpvp.api.base;
 
 
+import net.kissenpvp.api.database.ConnectionProvider;
 import net.kissenpvp.api.localization.GlobalLocaleRegistry;
 import org.jspecify.annotations.NonNull;
 
 public interface Kissen
 {
+    @NonNull ConnectionProvider connectionProvider();
+
     @NonNull GlobalLocaleRegistry localeRegistry();
 
     boolean started();
