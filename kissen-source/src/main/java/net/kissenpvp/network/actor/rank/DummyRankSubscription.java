e767c9c149f8243638d54cdb298bff14facedddd
Ivo Quiring <falk.quiring@gmail.com>
2025-09-27 10:19:01 +0200

    Add `DummyRankSubscription` implementation for default rank behavior

---
+++ kissen-source/src/main/java/net/kissenpvp/network/actor/rank/DummyRankSubscription.java
@@ -0,0 +1,63 @@
+package net.kissenpvp.network.actor.rank;
+
+import net.kissenpvp.api.network.actor.PlayerClient;
+import net.kissenpvp.api.network.actor.rank.Rank;
+import net.kissenpvp.api.network.actor.rank.RankSubscription;
+import net.kissenpvp.api.temporal.WritableTemporalObject;
+import net.kissenpvp.temporal.InternalWritableTemporalObject;
+import net.kissenpvp.temporal.timespan.PermanentTimeSpan;
+import org.jspecify.annotations.NonNull;
+
+import java.util.Optional;
+
+public class DummyRankSubscription implements RankSubscription
+{
+    private static final Rank DUMMY_RANK = new DummyRank();
+
+    private final PlayerClient playerClient;
+
+    public DummyRankSubscription(PlayerClient playerClient)
+    {
+        this.playerClient = playerClient;
+    }
+
+    @Override public @NonNull PlayerClient player() throws IllegalStateException
+    {
+        return playerClient;
+    }
+
+    @Override public @NonNull Optional<Rank> parent()
+    {
+        return Optional.of(DUMMY_RANK);
+    }
+
+    @Override public @NonNull String parentId()
+    {
+        return "dummy";
+    }
+
+    @Override public int parentSignature()
+    {
+        return 0;
+    }
+
+    @Override public @NonNull String id()
+    {
+        return "dummy";
+    }
+
+    @Override public int signature()
+    {
+        return 0;
+    }
+
+    @Override public boolean unsaved()
+    {
+        return false;
+    }
+
+    @Override public @NonNull WritableTemporalObject temporal()
+    {
+        return InternalWritableTemporalObject.toTemporal(new PermanentTimeSpan());
+    }
+}
